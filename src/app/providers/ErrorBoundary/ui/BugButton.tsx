e03874ef5d0de739fd6969826015ca8ae5d9fa64
Grigory Rodionov <grisha14423@gmail.com>
2024-09-16 13:07:21 +0300

    add Error Boundary, bug button for tests

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,19 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+
+// Компонент, который приводит к ошибке при нажатии на кнопку, для тестирования ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const throwError = () => setError(true);
+
+    useEffect(() => {
+        if (error) throw new Error();
+    }, [error]);
+
+    return (
+        <Button onClick={throwError}>
+            Throw error
+        </Button>
+    );
+};
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
